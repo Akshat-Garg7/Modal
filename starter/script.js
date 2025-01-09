886a7a05200c763763b7ba678e8a271674a49b74
Akshat-Garg7 <akshatgarg280503@gmail.com>
2025-01-09 22:15:15 +0530

    Updated script.js

--- starter/script.js
+++ starter/script.js
@@ -4,32 +4,23 @@ const modal=document.querySelector('.modal'); //prompt after button
 const close=document.querySelector('.close-modal'); // close button
 const overlay=document.querySelector('.overlay'); // blurry effect
 
+const add=function()
+{
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
 
-
+const remove =function()
+{
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
 for(let i=0;i<btnmodal.length;i++)
 {
-    btnmodal[i].addEventListener('click',
-        function()
-        {
-            modal.classList.remove('hidden');
-            overlay.classList.remove('hidden');
-        }
-    );
+    btnmodal[i].addEventListener('click',remove);
 }
-close.addEventListener('click',
-    function()
-    {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    }
-);
-overlay.addEventListener('click',
-    function()
-    {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    }
-);
+close.addEventListener('click',add);
+overlay.addEventListener('click',add);
 document.addEventListener('keydown',
     function(event)
     {
